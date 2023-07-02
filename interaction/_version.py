17d994b85dfb06a5a0a0e3a51c7be1fbc964e49a
David Juanwu Lu <juanwu@purdue.edu>
2023-07-02 04:45:12 -0400

    Bump version 0.1.1 -> 0.1.2

--- interaction/_version.py
+++ interaction/_version.py
@@ -1,4 +1,4 @@
 # Copyright (c) 2023, Juanwu Lu <juanwu@purdue.edu>.
 # Released under the BSD-3-Clause license.
 # See https://opensource.org/license/bsd-3-clause/ for licensing details.
-version = "0.1.0"
+version = "0.1.2"
