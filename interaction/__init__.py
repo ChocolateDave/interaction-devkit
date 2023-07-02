9fea4fb228b1b3876379ee003bae7e2ecab788ac
David Juanwu Lu <juanwu@purdue.edu>
2023-07-02 05:06:49 -0400

    Adds package fast-accessing

--- interaction/__init__.py
+++ interaction/__init__.py
@@ -4,11 +4,21 @@
 from __future__ import annotations
 
 from . import _version
+from .dataset import INTERACTIONCase, INTERACTIONMap, INTERACTIONScenario
 
-__version__ = _version.version
+__docformat__ = "restructuredtext"
 __doc__ = """
 interaction - INTERACTION dataset development toolkit.
 =============================================================
 
 This package provides a set of tools for working with the INTERACTION dataset.
 """
+__version__ = _version.version
+
+__all__ = [
+    "__version__",
+    "__doc__",
+    "INTERACTIONMap",
+    "INTERACTIONScenario",
+    "INTERACTIONCase",
+]
